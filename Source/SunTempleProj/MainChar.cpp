276ad30d2ae0caf589872f4712172cee5117d3c5
ISILV125 <igonildo7@gmail.com>
2023-04-07 01:32:45 -0300

    Improvements in character death scenario

--- Source/SunTempleProj/MainChar.cpp
+++ Source/SunTempleProj/MainChar.cpp
@@ -118,12 +118,27 @@ void AMainChar::IncrementCoins(int32 amount)
 
 void AMainChar::Die()
 {
+	if (MovementStatus == EMovementStatus::EMS_Dead) return;
 	UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
 
 	if (AnimInstance && CombatMontage) {
 		AnimInstance->Montage_Play(CombatMontage, 1.0f);
 		AnimInstance->Montage_JumpToSection(FName("Death"));
 	}
+
+	SetMovementStatus(EMovementStatus::EMS_Dead);
+}
+
+void AMainChar::Jump() {
+	if (MovementStatus != EMovementStatus::EMS_Dead) {
+		Super::Jump();
+	}
+
+}
+
+void AMainChar::DeathEnd() {
+	GetMesh()->bPauseAnims = true;
+	GetMesh()->bNoSkeletonUpdate = true;
 }
 
 // Called when the game starts or when spawned
@@ -141,6 +156,8 @@ void AMainChar::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 	float DeltaStamina = StaminaDrainRate * DeltaTime;
+
+	if (MovementStatus == EMovementStatus::EMS_Dead) return;
 
 	switch (StaminaStatus) {
 	case EStaminaStatus::ESS_Normal:
@@ -258,7 +275,7 @@ void AMainChar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 	PlayerInputComponent->BindAxis("LookUpRate", this, &AMainChar::LookUpAtRate);*/
 
 
-	PlayerInputComponent->BindAction("Jump", IE_Pressed, this,  &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this,  &AMainChar::Jump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 
 	PlayerInputComponent->BindAction("Sprint", IE_Pressed, this, &AMainChar::ShiftKeyDown);
@@ -272,7 +289,7 @@ void AMainChar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 void AMainChar::MoveForward(float value)
 {
-	if ((Controller != nullptr) && (value != 0.0f) && !bAttacking) {
+	if ((Controller != nullptr) && (value != 0.0f) && !bAttacking && (MovementStatus!=EMovementStatus::EMS_Dead)) {
 		//find out wich way is forward
 		const FRotator Rotation = Controller->GetControlRotation();
 		const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
@@ -284,7 +301,7 @@ void AMainChar::MoveForward(float value)
 
 void AMainChar::MoveRight(float value)
 {
-	if ((Controller != nullptr) && (value != 0.0f) && !bAttacking) {
+	if ((Controller != nullptr) && (value != 0.0f) && !bAttacking && (MovementStatus != EMovementStatus::EMS_Dead)) {
 		//find out wich way is forward
 		const FRotator Rotation = Controller->GetControlRotation();
 		const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
@@ -309,6 +326,8 @@ void AMainChar::LMBDown()
 {
 	
 	bLMBDown = true;
+
+	if (MovementStatus == EMovementStatus::EMS_Dead) return;
 
 	if (IsValid(ActiveOverlappingItem)) {
 		AWeapon* weapon = Cast<AWeapon>(ActiveOverlappingItem);
@@ -338,15 +357,15 @@ void AMainChar::SetEquippedWeapon(AWeapon* WeaponToSet)
 
 void AMainChar::Attack()
 {
-	if (bAttacking == true) return;
-	bAttacking = true;
-	SetInterpToEnemy(true);
+	if (!bAttacking && MovementStatus != EMovementStatus::EMS_Dead) {
+		bAttacking = true;
+		SetInterpToEnemy(true);
 
-	UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
+		UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
 
-	if (AnimInstance && CombatMontage) {
-		int32 section = FMath::RandRange(0, 1);
-		switch (section) {
+		if (AnimInstance && CombatMontage) {
+			int32 section = FMath::RandRange(0, 1);
+			switch (section) {
 			case 0:
 				AnimInstance->Montage_Play(CombatMontage, 2.2f);
 				AnimInstance->Montage_JumpToSection(FName("Attack_1"), CombatMontage);
@@ -358,7 +377,9 @@ void AMainChar::Attack()
 				break;
 			default:
 				break;
+			}
 		}
+
 	}
 	
 }
--- Source/SunTempleProj/MainChar.h
+++ Source/SunTempleProj/MainChar.h
@@ -11,6 +11,7 @@ UENUM(BlueprintType)
 enum class EMovementStatus : uint8 {
 	EMS_Normal UMETA(DisplayName="Normal"),
 	EMS_Sprinting UMETA(DisplayName = "Sprinting"),
+	EMS_Dead UMETA(DisplayName = "Dead"),
 
 	EMS_MAX UMETA(DisplayName = "DefaultMAX"),
 };
@@ -137,6 +138,8 @@ public:
 
 	void Die();
 
+	virtual void Jump() override;
+
 
 protected:
 	// Called when the game starts or when spawned
@@ -188,5 +191,8 @@ public:
 
 	UFUNCTION(BlueprintCallable)
 	void PlaySwingSound();
+
+	UFUNCTION(BlueprintCallable)
+		void DeathEnd();
 
 };
